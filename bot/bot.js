b530805b49f06a57e364efc8595403afb19db21c
berkeleyfx <bmartes98@gmail.com>
2022-09-23 09:48:58 +0700

    Updated bot

--- bot/bot.js
+++ bot/bot.js
@@ -5,16 +5,8 @@ const client = new Client({
     intents: [GatewayIntentBits.Guilds], partials: [Partials.Channel]
 });
 
-console.log(process.env.TOKEN)
-
 client.on("ready", () => {
     console.log("Bot launched");
-});
-
-client.on("messageCreate", (message) => {
-    if (message.content.startsWith("ping")) {
-      message.channel.send("pong!");
-    }
 });
 
 client.login(process.env.TOKEN);
