ff857f307c2cbe8f8b966bd0f8ae61f73981fd99
banz04 <berkeleyfx@gmail.com>
2022-12-20 09:11:40 +0700

    Add URL to template directory

--- src/mic_drop_results/constants.py
+++ src/mic_drop_results/constants.py
@@ -1,8 +1,9 @@
 import os
 
-# Web URLs
+# URLs
 REPO_URL = 'https://github.com/banz04/mic-drop-results/'
 LATEST_RELEASE_URL = f'{REPO_URL}releases/latest/'
+TEMPLATES_URL = f'{REPO_URL}tree/main/templates'
 
 # Local directories
 APP_DIR = os.path.dirname(os.path.realpath(__file__))
