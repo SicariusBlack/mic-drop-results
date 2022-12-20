838345a78205f6eb34589b834c4ed66bb17df59b
banz04 <berkeleyfx@gmail.com>
2022-12-20 22:27:12 +0700

    Fix paths of local directories

--- src/mic_drop_results/constants.py
+++ src/mic_drop_results/constants.py
@@ -7,5 +7,5 @@ TEMPLATES_URL = f'{REPO_URL}tree/main/templates'
 
 # Local directories
 APP_DIR = os.path.dirname(os.path.realpath(__file__))
-OUTPUT_DIR = f'{APP_DIR}output'
-AVATAR_DIR = f'{APP_DIR}avatars'
+OUTPUT_DIR = f'{APP_DIR}\\output'
+AVATAR_DIR = f'{APP_DIR}\\avatars'
