e9fa316667991a694b6b927303ea0a971229ebf4
banz04 <berkeleyfx@gmail.com>
2022-12-21 20:13:09 +0700

    Remove certain local directories

--- src/mic_drop_results/constants.py
+++ src/mic_drop_results/constants.py
@@ -1,11 +1,7 @@
 import os
 
-# URLs
 REPO_URL = 'https://github.com/banz04/mic-drop-results/'
 LATEST_RELEASE_URL = f'{REPO_URL}releases/latest/'
-TEMPLATES_URL = f'{REPO_URL}tree/main/templates'
+TEMPLATES_URL = f'{REPO_URL}tree/main/templates/'
 
-# Local directories
 APP_DIR = os.path.dirname(os.path.realpath(__file__))
-OUTPUT_DIR = f'{APP_DIR}\\output'
-AVATAR_DIR = f'{APP_DIR}\\avatars'
