9456d7f7bb8343cf5ece4ed77b53f098515f17ee
Banz <banz04@gmail.com>
2023-02-15 14:35:35 +0700

    Remove trailing slashes from URLs

--- src/mic_drop_results/constants.py
+++ src/mic_drop_results/constants.py
@@ -1,7 +1,7 @@
 import os
 
-REPO_URL = 'https://github.com/banz04/mic-drop-results/'
-LATEST_RELEASE_URL = f'{REPO_URL}releases/latest/'
-TEMPLATES_URL = f'{REPO_URL}tree/main/templates/'
+REPO_URL = 'https://github.com/banz04/mic-drop-results'
+LATEST_RELEASE_URL = f'{REPO_URL}/releases/latest'
+TEMPLATES_URL = f'{REPO_URL}/tree/main/templates'
 
 APP_DIR = os.path.dirname(os.path.realpath(__file__))
