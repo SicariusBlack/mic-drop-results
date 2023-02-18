bd3c8d2d12c791318c28243ed10cf96db6761474
Banz <banz04@gmail.com>
2023-02-18 19:50:38 +0700

    Major enhancement

--- src/mic_drop_results/compiled_regex.py
+++ src/mic_drop_results/compiled_regex.py
@@ -1,6 +1,7 @@
 import re
 
 
+# Section A: Find all
 # Match image URLs
 img_url_pattern = re.compile(
     r'(https?\:\/\/)?[\w\-\.]+\.[a-z]*\/'
@@ -10,10 +11,20 @@ img_url_pattern = re.compile(
 # Match 'field_name' from 'text containing {field_name} with more text behind'
 field_name_pattern = re.compile(r'(?<={)([\w \-]*?)(?=})')
 
-# Match space characters
-space_pattern = re.compile(r'\s')
-
 # Match 'username' from 'C:\Users\username\directory'
 username_pattern = re.compile(
     r'(?<=(?:\\|\/)Users(?:\\|\/)).+?(?=(?:\\|\/))',
     re.IGNORECASE)
+
+
+# Section B: Substitute
+# Match forbidden file name characters
+forbidden_char_pattern = re.compile(r'[\\\/:"*?<>|]+')
+
+# Match space characters
+space_pattern = re.compile(r'\s')
+
+
+# Section C: Full match
+# Match hex color value
+hex_pattern = re.compile(r'^[0-9a-f]{6}$', re.IGNORECASE)
